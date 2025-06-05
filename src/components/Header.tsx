b5d7097b8d6a9c340d5affc0c03cf9dd4b5666bc
rami kamel <ramikamel77@gmail.com>
2025-06-04 20:31:53 -0500

    changed header button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,7 @@ export const Header = () => {
                   size="sm" 
                   onClick={handleGoToApp}
                 >
-                  My App
+                  Enter Scores
                 </Button>
                 <Button 
                   variant="outline" 
